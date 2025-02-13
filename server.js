9e5c472b52b007d3565030ab7133347fc80055e5
sabrinaz35 <sj.zuurbier@gmail.com>
2025-02-13 12:37:46 +0100

    Extra uitleg bij de express code

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //Arrow function staat hier meteen al achter, als iemand een get request stuurt dan moet er hello world verstuurd worden.
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, () => { //Arrow function als hij aan het luisteren is dan console.logt hij wat hieronder staat.
   console.log(`Example app listening on port ${port}`)
 })
