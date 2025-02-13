90f5ef86559d6041503c5d87478666bb170330b7
sabrinaz35 <sj.zuurbier@gmail.com>
2025-02-13 13:24:55 +0100

    proberen 2.0

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000
 
 
 app.get('/', (req, res) => { //Arrow function staat hier meteen al achter, als iemand een get request stuurt dan moet er hello world verstuurd worden.
-  res.send('Olaa!')//res.send('<h1>Hello world<h1>')
+  res.send('<h1>Olaaa<h1>')//res.send('<h1>Hello world<h1>')
 })
 
 
